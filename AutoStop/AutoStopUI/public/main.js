f71932c9a9b4077678b09e3b6cefe38280447b1e
unknown <marko.hladjak@bitsorchestra>
2018-09-13 16:57:33 +0300

    scroling issue

--- AutoStop/AutoStopUI/public/main.js
+++ AutoStop/AutoStopUI/public/main.js
@@ -1,34 +1,40 @@
 //open_closeNav
-     
+
 function openNav() {
+    window.addEventListener('scroll', noscroll);
     if (screen.width < 600) {
-        
-    document.getElementById("mySidenav").style.width = "100%";
-    }
-    else {  
-    document.getElementById("mySidenav").style.width = "50%";
-    document.getElementById("mySidenav0").style.width = "0";
+        document.getElementById("co2").classList.add = "contt";
+        document.getElementById("mySidenav").style.width = "100%";
+    } else {
+        document.getElementById("mySidenav").style.width = "50%";
+        document.getElementById("mySidenav0").style.width = "0";
     }
 }
 
 function closeNav() {
+    window.removeEventListener('scroll', noscroll);
     document.getElementById("mySidenav").style.width = "0";
 }
-    
-    
-    
-    
+
+
+function noscroll() {
+    window.scrollTo(0, 0);
+}
+
+
+
 function openNav0() {
+    window.addEventListener('scroll', noscroll);
     if (screen.width < 600) {
-    document.getElementById("mySidenav0").style.width = "100%";   
-    }
-    else {
-    document.getElementById("mySidenav0").style.width = "50%";
-    document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("mySidenav0").style.width = "100%";
+    } else {
+        document.getElementById("mySidenav0").style.width = "50%";
+        document.getElementById("mySidenav").style.width = "0";
     }
 }
 
 function closeNav0() {
+    window.removeEventListener('scroll', noscroll);
     document.getElementById("mySidenav0").style.width = "0";
 }
 //open_closeNav
@@ -37,9 +43,9 @@ function myFunction() {
     var x = document.getElementById("demo");
     if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
-    } else { 
+    } else {
         x.className = x.className.replace(" w3-show", "");
-    } 
+    }
 }
 //Navigation bar
 //Slideshow
@@ -50,23 +56,23 @@ function carousel() {
     var i;
     var x = document.getElementsByClassName("mySlides");
     for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
+        x[i].style.display = "none";
     }
     myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 3000);    
+    if (myIndex > x.length) {
+        myIndex = 1
+    }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 3000);
 }
 //Slideshow
 // slow scroll
-$(".menu").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1000);
-    });
+$(".menu").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({
+        scrollTop: top
+    }, 1000);
+});
 // slow scroll
-
-
-
-
