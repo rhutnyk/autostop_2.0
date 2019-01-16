62fdc6db57659e091acc817838d4d1e0fe22e078
Ihor Moskvita <moskigo2@gmail.com>
2019-01-16 18:26:54 +0200

    Contact page for mobile

--- AutoStop/AutoStopUI/AUTO/src/components/ContactPage.tsx
+++ AutoStop/AutoStopUI/AUTO/src/components/ContactPage.tsx
@@ -13,18 +13,18 @@ export default class ContactPage extends Contact{
                 <div hidden={this.state.isEmailSend}>
 
                     <div className="row justify-content-center">
-                        <div className="col-11 col-sm-4 col-lg-3">
+                        <div className="col-11 col-md-4 col-lg-3">
                             <input className={this.state.nameValid ? "" : "error"} id="youname" type="text" placeholder="Ваше ім'я" value={this.state.name} onChange={(e) => { this.onChangeValue(e, { item: "name" }) }} /><br />
                             <small hidden={this.state.nameValid} className="errorLabel">введіть ваше ім'я</small>
                         </div>
 
-                        <div className="col-11 col-sm-4 col-lg-3"><input className={this.state.emailValid ? "" : "error"} id="mail" type="email" placeholder="Ваш E-mail" value={this.state.email} onChange={(e) => { this.onChangeValue(e, { item: "email" }) }} /><br />
+                        <div className="col-11 col-md-4 col-lg-3"><input className={this.state.emailValid ? "" : "error"} id="mail" type="email" placeholder="Ваш E-mail" value={this.state.email} onChange={(e) => { this.onChangeValue(e, { item: "email" }) }} /><br />
                             <small hidden={this.state.emailValid} className="errorLabel">введіть коректну пошту</small>
                         </div>
                     </div>
 
                     <div className="row justify-content-center">
-                        <div className="col-11 col-sm-8 col-lg-6">
+                        <div className="col-11 col-md-8 col-lg-6">
                             <textarea className={this.state.textValid ? "" : "error"} id="writetous" itemType="text" placeholder="Напишіть нам" value={this.state.text} onChange={(e) => { this.onChangeValue(e, { item: "text" }) }}></textarea>
                             <small hidden={this.state.textValid} className="errorLabel">напишіть нам</small>
                             <input id="send-mob" type="submit" value="відправити" onClick={this.onSend} />
