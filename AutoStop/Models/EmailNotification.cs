c845ecf4de75d36945b8de10e6e7d02d7fcc8c4b
Ihor Moskvita <moskigo2@gmail.com>
2019-01-22 17:37:30 +0200

    Add log if email sending are failed

--- AutoStop/Models/EmailNotification.cs
+++ AutoStop/Models/EmailNotification.cs
@@ -20,31 +20,39 @@ namespace AutoStop.Models
         {
             if (_name_user != String.Empty && _email_user != String.Empty && _body != String.Empty && _to != String.Empty)
             {
-                MailMessage mail = new MailMessage();
-                mail.To.Add(_to);
-                mail.From = new MailAddress("valen@gmail.com");
-                mail.Subject = _subject;
-                mail.Body = "<h3>Повідомлення надіслано сайтом <a href='http://autostop.bitsorchestra.com'>autostop.bitsorchestra.com</a></h3>" +
-                    "</hr>" +
-                    "<p>Імя: " + _name_user + "</p>" +
-                    "<p>Ел.пошта: " + _email_user + "</p>" +
-                    "<p>Повідомлення: <i>" + _body + "</i></p>";
-
-                mail.IsBodyHtml = true;
-
-                SmtpClient smpt = new SmtpClient();
-                smpt.Host = "smtp.gmail.com";
-                smpt.Credentials = new System.Net.NetworkCredential("valentynv22@gmail.com", "Tremolo2211");
-                smpt.Port = 587;
-                smpt.EnableSsl = true;
-
                 try
                 {
+
+                    MailMessage mail = new MailMessage();
+                    mail.To.Add(_to);
+                    mail.From = new MailAddress("valentyn@gmail.com");
+                    mail.Subject = _subject;
+                    mail.Body = "<h3>Повідомлення надіслано сайтом <a href='http://autostop.bitsorchestra.com'>&laquo;autostop.bitsorchestra.com&raquo;</a></h3>" +
+                                "</hr>" +
+                                "<p>Імя: " + _name_user + "</p>" +
+                                "<p>Ел.пошта: " + _email_user + "</p>" +
+                                "<p>Повідомлення: <i>" + _body + "</i></p>";
+
+                    mail.IsBodyHtml = true;
+
+                    SmtpClient smpt = new SmtpClient();
+                    smpt.Host = "smtp.gmail.com";
+                    smpt.Credentials = new System.Net.NetworkCredential("valentynv22@gmail.com", "Tremolo2211");
+                    smpt.Port = 587;
+                    smpt.EnableSsl = true;
+
+
                     smpt.Send(mail);
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine("Error - " + ex.Message);
+                    Log log = new Log()
+                    {
+                        LogDate = DateTime.Now,
+                        Message = ex.Message,
+                        FileName = "Email notification"
+                    };
+                    new WorkWithData().AddLog(log);
                 }
             }
         }
